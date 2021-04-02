a5940f28cbec6f4a0b2d01a898f04a1d8f03c226
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-04-02 05:48:43 +0000

    error handling

--- pages/api/image.js
+++ pages/api/image.js
@@ -2,14 +2,29 @@
 const filter = require("lodash").filter;
 
 export default async (req, res) => {
-  let cameras = (
-    await fetch(
-      `https://api.data.gov.sg/v1/transport/traffic-images?date_time=${req.query.time.substring(0, 19)}`
-    ).then((r) => r.json())
-  ).items[0].cameras;
+  let cameras;
+  let failed = true;
+
+  while (failed == true) {
+    console.log("this ran");
+    failed = false
+    cameras = await fetch(
+      `https://api.data.gov.sg/v1/transport/traffic-images?date_time=${req.query.time.substring(
+        0,
+        19
+      )}`
+    )
+      .then((r) => r.json())
+      .catch(function () {
+        console.log("error");
+        failed = true
+      });
+  }
+
+  cameras = cameras.items[0].cameras;
   let targetCamera = filter(
     cameras,
     (camera) => camera["camera_id"] === "2701"
   );
-  res.redirect(targetCamera[0].image)
-}
+  res.redirect(targetCamera[0].image);
+};
